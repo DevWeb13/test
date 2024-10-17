6abb95611f4726f5163fdc1d7f26a4567c4426fa
DevWeb13 <giuliano.loic@sfr.fr>
2024-10-17 12:03:32 +0200

    ttttttttttt

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -11,61 +11,8 @@ import { setupServiceWorker } from '@builder.io/qwik-city/service-worker';
 
 setupServiceWorker();
 
-self.addEventListener('install', (event: ExtendableEvent) => {
-  event.waitUntil(
-    caches
-      .open('offline-cache')
-      .then((cache) => {
-        return cache.addAll([
-          '/', // La page principale
-          '/build/*', // Les fichiers de build
-        ]);
-      })
-      .catch((error) => {
-        console.error('Erreur lors du cache des fichiers:', error);
-      })
-  );
-  self.skipWaiting();
-});
+addEventListener('install', () => self.skipWaiting());
 
-self.addEventListener('activate', (event: ExtendableEvent) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== 'offline-cache') {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event: FetchEvent) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => {
-        if (response) {
-          return response; // Si une réponse est trouvée dans le cache, on la renvoie
-        }
-        return fetch(event.request).catch(() => {
-          // Si la requête réseau échoue, on renvoie la page offline
-          return caches.match('/offline.html').then((offlineResponse) => {
-            return (
-              offlineResponse ||
-              new Response('Offline page not found', { status: 404 })
-            );
-          });
-        });
-      })
-      .catch((error) => {
-        console.error('Erreur lors de la gestion de fetch:', error);
-        return new Response('Erreur réseau', { status: 500 });
-      })
-  );
-});
+addEventListener('activate', () => self.clients.claim());
 
 declare const self: ServiceWorkerGlobalScope;
